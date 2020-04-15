eff281598aca4d40f6baa18fec66504a5cd88b33
Adeilson Silva <adeilson.129@gmail.com>
2020-04-15 14:40:46 -0300

    Retrabalho na conexão e adicionado a classe de consulta ao banco de dados

--- scripts/model/connection.js
+++ scripts/model/connection.js
@@ -1,15 +1,24 @@
-module.exports = () => {
-    const mysql = require('mysql');
+const mysql = require('mysql');
 
-    const conect = mysql.createConnection({
-        host:'LocalHost',
-        port: 3306,
-        user: 'root',
-        password: '',
-        database: 'resttable'
-    });
-    conect.connect(function(err){
-        if(err) return console.log(err);
-        console.log('Conexão Realizada com Sucesso');
-    });
-};
+const conect = mysql.createConnection({
+    host:'LocalHost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'resttable'
+});
+conect.connect(function(err){
+    if(err) return console.log(err);
+    console.log('Conexão Realizada com Sucesso');
+});
+
+function consultTable(conn){
+    if(conect){
+        const sqlCommand = "select * from resttable.cadastro;";
+
+        conn.query(sqlCommand, function(err){
+            if(err) console.log('Erro ao procurar Tabela');
+            console.log('Consulta realizada com sucesso!')
+        });
+    }
+}
