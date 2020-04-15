079a792327cba8515c12bbaeb368f8c8f4ccc15e
Adeilson Silva <adeilson.129@gmail.com>
2020-04-15 17:19:59 -0300

    Criando a API

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const consign = require('consign');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+const mysql = require('mysql');
+
+//configurando Parser para futuros POSTS
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+consign().include('routes').into(app);
+
+app.listen(port);
+console.log('API FUNCIONANDO!');
